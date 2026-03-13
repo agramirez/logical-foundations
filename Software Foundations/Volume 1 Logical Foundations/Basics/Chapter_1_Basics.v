3158707d8d938f1cb91484162a5ebf52d43ae3db
Alex Ramirez <alexandergramirez@gmail.com>
2026-03-13 11:59:56 -0500

    Updates to basics

--- Software Foundations/Volume 1 Logical Foundations/Basics/Chapter_1_Basics.v
+++ Software Foundations/Volume 1 Logical Foundations/Basics/Chapter_1_Basics.v
@@ -3,3 +3,32 @@
     Description:    Problems and solutions for the Basics chapter of Logical
                     Foundations book.
 ******************************************************************************)
+Inductive day : Type :=
+  | monday
+  | tuesday
+  | wednesday
+  | thursday
+  | friday
+  | saturday
+  | sunday.
+
+Definition next_working_day (d:day) : day :=
+    match d with
+        | monday => tuesday
+        | tuesday => wednesday
+        | wednesday => thursday
+        | thursday => friday
+        | friday => monday
+        | saturday => monday
+        | sunday => monday
+    end.
+
+Compute (next_working_day friday).
+Compute (next_working_day (next_working_day saturday)).
+
+Example test_nwd1: next_working_day monday = tuesday.
+
+Example test_next_working_day:
+    (next_working_day (next_working_day saturday)) = tuesday.
+ Proof. simpl. reflexivity. Qed.
+
