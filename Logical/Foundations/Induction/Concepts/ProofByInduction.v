e69381f286acd105affed350242ca3fc6e67d31f
Alex Ramirez <alexandergramirez@gmail.com>
2026-04-09 00:57:21 -0500

    Completed two exercises in the Induction chapter and added new concepts in the same

---
+++ Logical/Foundations/Induction/Concepts/ProofByInduction.v
@@ -0,0 +1,49 @@
+(** *Proof by Induction Chapter
+
+    https://softwarefoundations.cis.upenn.edu/lf-current/Induction.html
+*)
+From LF Require Export ProofByCaseAnalysis.
+
+Theorem add_0_r_firsttry : forall n:nat,
+    n + 0 = n.
+Proof.
+    intros n.
+    simpl. (* Does nothing! *)
+Abort.
+
+Theorem add_0_r_secondtry : forall n:nat,
+    n + 0 = n.
+Proof.
+    intros n. destruct n as [| n'] eqn:E.
+    - (* n = 0 *)
+    reflexivity. (* so far so good... *)
+    - (* n = S n' *)
+    simpl. (* ...but here we are stuck again *)
+Abort.
+
+Theorem add_0_r : forall n:nat, 
+    n + 0 = n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *) reflexivity.
+  - (* n = S n' *) simpl. rewrite -> IHn'. reflexivity. 
+Qed.
+
+Theorem minus_n_n : forall n,
+    minus n n = 0.
+Proof.
+    (* WORKED IN CLASS *)
+    intros n. induction n as [| n' IHn'].
+    - (* n = 0 *)
+    simpl. reflexivity.
+    - (* n = S n' *)
+    simpl. rewrite -> IHn'. reflexivity. 
+Qed.
+
+Fixpoint double (n:nat) :=
+    match n with
+        | O => O
+        | S n' => S (S (double n'))
+    end.
+
+Check double.
