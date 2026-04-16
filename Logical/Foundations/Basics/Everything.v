a12bd44b29110c8ee461d04188463288749eae57
Alex Ramirez <alexandergramirez@gmail.com>
2026-04-16 21:52:46 +0000

    Refactored exports to include full path so no ambiguities arise

--- Logical/Foundations/Basics/Everything.v
+++ Logical/Foundations/Basics/Everything.v
@@ -3,20 +3,20 @@
     This file serves as a collector for all sections and exercises in the chapter.  
     It can be used to import all of the work completed.
 *)
-Require Export Concepts.DataAndFunctions.
-Require Export Concepts.ProofBySimplification.
-Require Export Concepts.ProofByRewriting.
-Require Export Concepts.ProofByCaseAnalysis.
+Require Export LF.Basics.Concepts.DataAndFunctions.
+Require Export LF.Basics.Concepts.ProofBySimplification.
+Require Export LF.Basics.Concepts.ProofByRewriting.
+Require Export LF.Basics.Concepts.ProofByCaseAnalysis.
 
-Require Export Exercises.AndB3.
-Require Export Exercises.AndBTrueElim2.
-Require Export Exercises.Factorial.
-Require Export Exercises.LtB.
-Require Export Exercises.MoreExercises.
-Require Export Exercises.MultN1.
-Require Export Exercises.NandB.
-Require Export Exercises.PlusIdExercise.
-Require Export Exercises.ZeroNbeQPlus1.
+Require Export LF.Basics.Exercises.AndB3.
+Require Export LF.Basics.Exercises.AndBTrueElim2.
+Require Export LF.Basics.Exercises.Factorial.
+Require Export LF.Basics.Exercises.LtB.
+Require Export LF.Basics.Exercises.MoreExercises.
+Require Export LF.Basics.Exercises.MultN1.
+Require Export LF.Basics.Exercises.NandB.
+Require Export LF.Basics.Exercises.PlusIdExercise.
+Require Export LF.Basics.Exercises.ZeroNbeQPlus1.
 
 (* LateDays required a special definition of <? so we 
 won't export it here since it conflicts with the standard
